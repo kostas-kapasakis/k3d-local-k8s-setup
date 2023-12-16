af36fc98b51cb09592939f8269b7dcefbe66197d
Kostas Kapasakis <k.kapasakis@interworks.cloud>
2023-12-16 23:40:11 +0200

    refactor deploy.sh

--- AppDeployment/deploy.sh
+++ AppDeployment/deploy.sh
@@ -3,23 +3,29 @@
 ###
 # Replace environment variables
 ##
-source ./replace_env.sh .deploymentyamlfilefolder ./env.txt
+replace_environment_variables(){
+	source ./replace_env.sh .deploymentyamlfilefolder ./env.txt
+}
 
 ###
 # Build and push docker image to local registry
 ###
-
-docker build -t localhost:55555/app:1.0.0 -f ../../src/Services/App/app/Dockerfile ../../
-docker push localhost:55555/app:1.0.0
-
-# either create a new random number and add it as a image version while tagging 
-# then need to replace the image version in the env.txt
+build_and_push_docker_image(){
+  docker build -t localhost:55555/app:1.0.0 -f ../../src/Services/App/app/Dockerfile ../../
+  docker push localhost:55555/app:1.0.0
+}
 
 ###
 # Deploy k8s objects
 ###
 
-kubectl apply -f ./secret.yml
-kubectl apply -f ./service.yml
-kubectl apply -f ./ingress.yml
-kubectl apply -f ./deployment.yml
+deploy_app_files(){
+  kubectl apply -f ./api/secret.yml
+  kubectl apply -f ./api/service.yml
+  kubectl apply -f ./api/ingress.yml
+  kubectl apply -f ./api/deployment.yml
+}
+
+replace_environment_variables
+build_and_push_docker_image
+deploy_app_files

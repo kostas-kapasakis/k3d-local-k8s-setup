3495c5dbde1a8b4eb874c01cc1f8d46c573a8696
Kostas Kapasakis <k.kapasakis@interworks.cloud>
2023-12-18 22:44:35 +0200

    Create folders per deployment solution

---
+++ AppDeployment/deploy_with_kustomize/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###
+# Replace environment variables
+##
+replace_environment_variables(){
+	source ./replace_env.sh .deploymentyamlfilefolder ./env.txt
+}
+
+###
+# Build and push docker image to local registry
+###
+build_and_push_docker_image(){
+  docker build -t localhost:55555/app:1.0.0 -f ../../src/Services/App/app/Dockerfile ../../
+  docker push localhost:55555/app:1.0.0
+}
+
+###
+# Deploy k8s objects
+###
+
+deploy_app_files(){
+  kubectl apply -f ./api/secret.yml
+  kubectl apply -f ./api/service.yml
+  kubectl apply -f ./api/serviceMonitor.yml
+  kubectl apply -f ./api/deployment.yml
+}
+
+replace_environment_variables
+build_and_push_docker_image
+deploy_app_files
